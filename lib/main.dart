f3c93af0e9ce9786e07b19530d91d4626637df94
Nguyễn Hữu Hòa <68727271+vjetgolang@users.noreply.github.com>
2021-07-14 00:37:04 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:weather/currentWeather.dart';
+import 'package:weather/models/location.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  List<Location> locations = [
+    new Location(
+        city: "Tỉnh Tây Ninh", country: "VN", lat: "11.3333", lon: "106.1667")
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: CurrentWeatherPage(locations, context),
+    );
+  }
+}
